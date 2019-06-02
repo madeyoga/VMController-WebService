d2770269050ad8f4d74bcec8b4353ba93ec3f296
MadeYoga <vngla21@gmail.com>
2019-06-03 00:13:23 +0700

    Add VM Info & RunScript feature.

--- vmcservice/urls.py
+++ vmcservice/urls.py
@@ -5,8 +5,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='vmcindex'),
+    path('home/', views.index, name='vmchome'),
+    url(r'^detail/(?P<vm_id>\d+)$', views.vm_detail, name='vmcdetail'),
     path('ajax/vms', views.virtual_machines, name='vmcvms'),
     path('login/', views.login_view, name='vmclogin'),
     url(r'^logout/$', views.logout_view, name='vmclogout'),
+
     url(r'^ajax/vmpower/(?P<vm_id>\d+)/(?P<todo>\D+)$', views.vmpower, name='vmcvmpower'),
+    url(r'^ajax/clone/(?P<vm_id>\d+)/(?P<todo>\D+)/(?P<clone_name>\D+)$', views.clone, name='vmclone'),
+    url(r'^ajax/delete/(?P<vm_id>\d+)$', views.delete, name='vmdelete'),
+    url(r'^ajax/info/(?P<vm_id>\d+)$', views.info, name='vmcinfo'),
+    url(r'^ajax/runscript/(?P<vm_id>\d+)$', views.run_script, name='vmcrunscript'),
 ]
--- vmcservice/views.py
+++ vmcservice/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from .models import VirtualMachine
 from django.core import serializers
 
@@ -9,6 +9,7 @@ from django.contrib import messages
 
 import subprocess
 import json
+import os
 
 @login_required(login_url='/vmc/login/')
 def index(request):
@@ -23,6 +24,20 @@ def index(request):
     }
     return render(request, 'vmcservice/index.html', vms_dictionary)
 
+@login_required(login_url='/vmc/login/')
+def vm_detail(request, vm_id):
+    """
+    Virtual Machine detail page.
+    to get the virtual machine current information.
+    such as memory usage. Online time. etc.
+    """
+    
+    vm = VirtualMachine.objects.get(id=vm_id, user=request.user)
+    dictionary = {
+        'vm': vm
+    }
+    return render(request, "vmcservice/detail.html", dictionary)
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -30,14 +45,14 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return index(request)
+            return HttpResponseRedirect('/vmc/')
         else:
             messages.error(request, 'username or password not correct')
     return render(request, 'vmcservice/login.html')
 
 def logout_view(request):
     logout(request)
-    return index(request)
+    return HttpResponseRedirect('/vmc/login/')
 
 def virtual_machines(request):
     """
@@ -45,7 +60,7 @@ def virtual_machines(request):
     url: localhost:8000/vmc/ajax/vms
     """
 
-    data = serializers.serialize('json', VirtualMachine.objects.all())
+    data = serializers.serialize('json', VirtualMachine.objects.get(user=request.user))
     return HttpResponse(data, content_type='application/json')
 
 def vmpower(request, vm_id, todo):
@@ -81,6 +96,8 @@ def clone(request, vm_id, todo, clone_name):
     """
     views to clone a virtual machine.
     url: localhost:8000/vmc/ajax/clone/<vm-id>/[linked|full]/<vm-clone-name>
+
+    Clone.exe <linked|full> <vmx-path> <clone-path>
     """
 
     vm = VirtualMachine.objects.get(id=vm_id, user=request.user)
@@ -109,6 +126,8 @@ def delete(request, vm_id):
     """
     views to delete a virtual machine
     url: localhost:8000/vmc/ajax/delete/<vm-id>
+
+    Clone.exe DELETE <vmx-path>
     """
 
     vm = VirtualMachine.objects.get(id=vm_id, user=request.user)
@@ -130,5 +149,85 @@ def delete(request, vm_id):
         'stdout' : output.stdout,
         'stderr' : output.stderr,
         'status' : todo
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def info(request, vm_id):
+    """
+    views to get virtual-machine's variables
+    variables: Ip Address, Memory Usage, Disk Space usage.
+
+    Info.exe <vmx-path>
+    """
+
+    vm = VirtualMachine.objects.get(id=vm_id, user=request.user)
+    args = [
+        'G:\\Kuliah\\github\\VMControlWebService\\vmcservice\\GuestOps\\Debug\\Info.exe',
+        vm.vmx_path
+    ]
+    output = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    print(output.returncode, output.stdout, output.stderr)
+
+    ipadd = '-'
+    memory = '-'
+    disk = '-'
+    # read disk1.txt, ipadd.txt, mem.txt file
+    with open('./vmcservice/GuestOps/Debug/ipadd.txt', 'r') as f:
+        ipadd = f.readline()
+
+    with open('./vmcservice/GuestOps/Debug/mem.txt', 'r') as f:
+        memory = f.readline()
+        memory = memory.split()
+        memory_total = memory[1]
+        memory_used = memory[2]
+        memory_available = memory[6]
+
+    with open('./vmcservice/GuestOps/Debug/disk1.txt', 'r') as f:
+        disk = f.readline()
+        disk = disk.split()
+        disk_total = disk[1]
+        disk_used = disk[2]
+        disk_available = disk[3]
+
+    data = {
+        'vmid': vm.id,
+        'vmname': vm.name,
+        'returncode': output.returncode,
+        'stdout': output.stdout,
+        'stderr': output.stderr,
+        'ipaddress': ipadd.rstrip(),
+        'used_memory': memory_used,
+        'available_memory': memory_available,
+        'total_memory': memory_total,
+        'total_disk': disk_total,
+        'used_disk': disk_used,
+        'available_disk': disk_available
+    }
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def run_script(request, vm_id):
+    """
+    views to run script in guest virtual machine.
+    url: localhost:8000/vmc/ajax/runscript/<vm-id>
+    """
+
+    script_text = request.GET.get('scriptText')
+    interpreter = request.GET.get('interpreter')
+
+    vm = VirtualMachine.objects.get(id=vm_id, user=request.user)
+    args = [
+        'G:\\Kuliah\\github\\VMControlWebService\\vmcservice\\GuestOps\\Debug\\RunScript.exe',
+        vm.username, vm.password, vm.vmx_path, interpreter, script_text
+    ]
+    output = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    print(output.returncode, output.stdout, output.stderr)
+
+    data = {
+        'vmid'   : vm.id,
+        'vmname'     : vm.name,
+        'returncode' : output.returncode,
+        'stdout' : output.stdout,
+        'stderr' : output.stderr
     }
     return HttpResponse(json.dumps(data), content_type='application/json')
