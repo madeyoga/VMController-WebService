909432a22c1e96bb5127d57c30b61b1c79c1eec4
MadeYoga <vngla21@gmail.com>
2019-06-03 15:11:40 +0700

    Add operating system name in Virtual Machine model.

--- vmcservice/models.py
+++ vmcservice/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class VirtualMachine(models.Model):
     """
     One user has many Virtual Machine.
+    One virtual machine owned by one user.
     """
 
     user     = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -12,6 +13,7 @@ class VirtualMachine(models.Model):
     password = models.CharField(max_length = 32 , default = '')
     vmx_path = models.CharField(max_length = 128, default = '')
     status   = models.CharField(max_length = 8  , default = 'off')
+    operating_system = models.CharField(max_length = 128, default = 'Ubuntu Server 18.04.2 LTS')
     
     def __str__(self):
         return self.name + " " + self.vmx_path + " " + self.status
